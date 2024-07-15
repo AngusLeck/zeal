27e725e7b20922f96604a880fa9bb076d74916cd
angus leck <aleck42@gmail.com>
2024-07-15 21:22:13 +1000

    fix: import

--- public/main.ts
+++ public/main.ts
@@ -26,15 +26,13 @@ let paused = false;
 let speed = 1;
 
 const loader = new THREE.TextureLoader();
-const jupiterTexture = loader.load("./public/jupiter/textures/texture.jpeg");
+const jupiterTexture = loader.load("/jupiter/textures/texture.jpeg");
 
 const jupiterMaterial = new THREE.MeshPhysicalMaterial({
   map: jupiterTexture,
 });
 
-const earthTexture = loader.load(
-  "./public/earth/textures/Material.001_baseColor.jpeg"
-);
+const earthTexture = loader.load("/earth/textures/Material.001_baseColor.jpeg");
 
 const earthMaterial = new THREE.MeshPhysicalMaterial({
   map: earthTexture,
